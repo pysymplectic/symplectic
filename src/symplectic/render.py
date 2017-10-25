f5a2653a56ada8f776046e2b9cf70a056f6fe057
Moshe Zadka <zadka.moshe@gmail.com>
2017-10-25 08:22:08 -0700

    temp commit

--- src/symplectic/render.py
+++ src/symplectic/render.py
@@ -3,10 +3,7 @@ import shutil
 
 import chameleon
 
-def _process(template_file, inputs, output_file):
-    with open(template_file) as fp:
-        template_str = fp.read()
-    template = chameleon.PageTemplate(template_str)
+def _process(template, inputs, output_file):
     rendered = template(**inputs)
     with open(output_file, 'w') as fp:
         fp.write(rendered)
@@ -14,16 +11,17 @@ def _process(template_file, inputs, output_file):
 def render(blog, theme, output):
     if not os.path.exists(output):
         os.makedirs(output)
-    _process(os.path.join(theme, 'river.html'),
+    loader = chameleon.PageTemplateLoader(theme)
+    _process(loader['river.html'],
              dict(metadata=blog.metadata, posts=blog.posts,
                   archives=[dict(name='All', link='list.html')]),
              os.path.join(output, 'river.html'))
-    _process(os.path.join(theme, 'list.html'),
+    _process(loader['list.html'],
              dict(metadata=blog.metadata, posts=blog.posts,
                   archives=[dict(name='All', link='list.html')]),
              os.path.join(output, 'list.html'))
     for i, post in enumerate(blog.posts):
-        _process(os.path.join(theme, 'post.html'),
+        _process(loader['post.html'],
                  dict(metadata=blog.metadata, post=post),
                  os.path.join(output, '{}.html'.format(post.slug)))
     for asset in ['css', 'js']:
