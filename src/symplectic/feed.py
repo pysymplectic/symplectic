91f8db1ae06f77bdab740bb9c61a58b2169022b9
Moshe Zadka <zadka.moshe@gmail.com>
2017-12-12 18:33:22 -0800

    frop

--- src/symplectic/feed.py
+++ src/symplectic/feed.py
@@ -1,10 +1,16 @@
 from xml.etree import ElementTree as ET
 
+def to_timestamp(dt_obj):
+    dt_obj = dt_obj.astimezone(UTC)
+    return dt_obj.strftime('%Y-%m-%dT%H:%M:%SZ')
+
 def _generate_atom(blog, fp):
-    root = ET.Element('feed')    
+    root = ET.Element('feed',
+                      xmlns="http://www.w3.org/2005/Atom")
     ET.SubElement(root, 'id').text = blog.metadata.base
     ET.SubElement(root, 'title').text = blog.metadata.title
     ET.SubElement(root, 'subtitle').text = blog.metadata.description
+    ET.SubElement(root, 'updated').text = to_timestamp(datetime.datetime.now())
     ET.SubElement(root, 'link', rel='alternate', type='text/html',
                                 href=blog.metadata.base)
     gen = ET.SubElement(root, 'generator', uri='http://github.com/pysymplectic')
